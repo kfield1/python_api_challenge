eb2f35a7efc06661c8ea08b3230ee23efc665d49
kfield1 <kayt1303@yahoo.com>
2021-03-03 19:36:24 -0600

    start homework

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "3e02b405ead2c074fdb666ca9e6f4af3"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBZj_3Sx8dQPX5_APLXJYR9-A8WfdfQi9w"
